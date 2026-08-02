062f6a2a6058f551feaefca9ad9524f422da2a77
rawidaniel <rawidaniel96@gmail.com>
2026-08-02 18:38:50 +0300

    feat: add custom global exception handler

---
+++ src/http/common/app-exception.filter.ts
@@ -0,0 +1,29 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import { DomainError } from '../../domain/domain-errors';
+
+@Catch()
+export class AppExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const reply = ctx.getResponse<FastifyReply>();
+    const request = ctx.getRequest<FastifyRequest>();
+
+    const requestId = request.id;
+
+    if (exception instanceof DomainError) {
+      reply
+        .status(exception.httpStatus)
+        .send(exception.toSafeResponse(requestId));
+      return;
+    }
+
+    reply.status(500).send({
+      error: {
+        code: 'INTERNAL_ERROR',
+        message: 'An unexpected error occurred.',
+        requestId,
+      },
+    });
+  }
+}
