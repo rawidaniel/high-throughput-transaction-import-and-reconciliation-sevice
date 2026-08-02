b788960be21371ab486354e0607a22a9c4563095
rawidaniel <rawidaniel96@gmail.com>
2026-08-02 18:40:41 +0300

    feat: add custoem global exception filter in the provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AppExceptionFilter } from './http/common/app-exception.filter';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: AppExceptionFilter },
+  ],
 })
 export class AppModule {}
