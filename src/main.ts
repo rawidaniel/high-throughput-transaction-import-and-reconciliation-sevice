1268634d1f303a9f3eb7ef87952fcd90487397cd
rawidaniel <rawidaniel96@gmail.com>
2026-08-02 18:42:33 +0300

    feat: use fastfy adaptor to create the server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { randomUUID } from 'node:crypto';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const adapter = new FastifyAdapter({
+    genReqId: () => randomUUID(),
+  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
